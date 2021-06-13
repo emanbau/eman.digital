9cb93fc5ff942dfc5de42bf24c41211a5e075e90
Eman Bau <emanbautista410@gmail.com>
2021-06-12 23:32:40 -0400

    About & Work Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.scss';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Switch>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Work.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Work: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Work;
