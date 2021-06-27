e18059871704ec3de1fd5c2f52e5443a132bf141
Eman Bau <emanbautista410@gmail.com>
2021-06-27 10:14:42 -0400

    Three.js Component Init

---
+++ src/components/ThreeImage.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement, useEffect, useRef } from 'react';
+import * as THREE from 'three';
+
+interface Props {
+    
+}
+
+const ThreeImage = React.memo(function ThreeImage({}: Props): ReactElement {
+    const threeRef = useRef();
+    useEffect(() => {
+        
+
+        return() => {
+
+        }
+    });
+
+    return (
+        <div>
+            
+        </div>
+    )
+});
+
+export default ThreeImage
