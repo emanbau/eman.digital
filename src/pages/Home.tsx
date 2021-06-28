c2dad031068c4834329de2c38c2318f6d147692b
Eman Bau <emanbautista410@gmail.com>
2021-06-28 00:41:05 -0400

    nav link init

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,10 @@ import ThreeImage from '../components/ThreeImage';
 const Home: React.FC = () => {
     return (
         <div className="home-container-dark">
-            <h1 className="work-nav">work</h1>
-            <h1 className="about-nav">about</h1>
+            <h1 className="work-nav">WORK</h1>
+            <h1 className="about-nav">ABOUT</h1>
+            <h1 className="contact-nav">CONTACT</h1>
+            <h1 className="blog-nav">©2021</h1>
             <ThreeImage />
             <h1 className="header">
                 DEVELOPER
