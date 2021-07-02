3e53c754f4b6014ee11d25d381140de9a4af9024
Eman Bau <emanbautista410@gmail.com>
2021-07-01 22:44:55 -0400

    Three Grain Filter Init

---
+++ src/components/ThreeGrain.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement, useRef, useEffect } from 'react'
+
+
+
+function ThreeGrain(): ReactElement {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ThreeGrain
