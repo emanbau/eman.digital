6f25a71dc4baee07b8ae88fd64fd21ee9749fcd5
Eman Bau <emanbautista410@gmail.com>
2021-07-06 11:23:49 -0400

    CSS Grain Filter

---
+++ src/components/GrainFilter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.scss';
+
+
+const GrainFilter = React.memo(function GrainFilter() {
+
+
+    return (
+        <div className="three-grain" />
+    )
+})
+
+export default GrainFilter
